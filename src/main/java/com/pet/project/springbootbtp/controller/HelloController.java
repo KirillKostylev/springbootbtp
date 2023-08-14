64196967f7b91a0ced249e90d2b68776930806e2
kiryl.kostyleu <kiryl.kostyleu@sap.com>
2023-08-14 13:50:22 +0300

    Add helloworld and web apps with xsuaa

---
+++ src/main/java/com/pet/project/springbootbtp/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.pet.project.springbootbtp.controller;
+
+
+import com.pet.project.springbootbtp.exceptions.NotAuthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.sap.cloud.security.xsuaa.token.Token;
+
+
+@RestController
+@RequestMapping(path = "")
+public class HelloController {
+
+    @GetMapping(path = "")
+    public ResponseEntity<String> readAll(@AuthenticationPrincipal Token token) {
+        if (!token.getAuthorities().contains(new SimpleGrantedAuthority("Display"))) {
+            throw new NotAuthorizedException("This operation requires \"Display\" scope");
+        }
+
+        return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
+    }}
---
+++ src/test/java/com/pet/project/springbootbtp/SpringbootbtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.project.springbootbtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
